c920a1ffa3f0277f1d0930bbd193a62f0cffd5bf
Mugsy Carter <mugsycarter@gmail.com>
2017-11-18 14:49:24 -0800

    trip controller added

---
+++ app/src/components/trip/trip.js
@@ -0,0 +1,14 @@
+import template from './trip.html';
+
+export default {
+    template,
+    controller
+};
+
+controller.$inject = ['$timeout', '$rootScope'];
+
+function controller(timeout) {
+    this.options = {
+        
+    };
+}
