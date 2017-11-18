feb53908c383d9840872a72b521e136e4e826604
Mugsy Carter <mugsycarter@gmail.com>
2017-11-18 14:53:16 -0800

    home controller added

---
+++ app/src/components/home/home.js
@@ -0,0 +1,33 @@
+import template from './home.html';
+
+export default {
+    template,
+    controller
+};
+
+
+controller.$inject = ['$timeout'];
+
+function controller($timeout) {
+
+    this.letters=[];
+    this.letterOptions = [['I', 'E'],['N', 'S'],['T','F'],['J','P']];
+
+    this.letters = [];
+    this.count = 0;
+  
+
+
+    this.update = ()=>{
+        if (this.count < 1000){
+            for(let i=0;i<this.letterOptions.length; i++){
+                this.letters[i]= {letter: this.letterOptions[i][Math.round(Math.random())]};
+            }
+            this.count ++;
+            $timeout(this.update, 1000);
+        }
+    };
+
+    this.update();
+
+};
